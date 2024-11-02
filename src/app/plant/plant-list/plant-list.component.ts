a813309e1258825c94a5f92aebd2c766926f739b
daniela sanchez <daniela.sancheza@perficient.com>
2024-11-02 14:50:56 -0500

    Tarea: mostrar total de plantas por tipo

--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -9,11 +9,15 @@ import { Plant } from '../plant';
 })
 export class PlantListComponent implements OnInit {
   plants: Plant[] | undefined;
+  interiorPlants: Plant[] | undefined;
+  exteriorPlants: Plant[] | undefined;
   constructor(private readonly plantService:PlantService) { }
 
   ngOnInit() {
     this.plantService.getPlants().subscribe(plants => {
       this.plants = plants;
+      this.interiorPlants = this.plants.filter(plant => plant.tipo === 'Interior');
+      this.exteriorPlants = this.plants.filter(plant => plant.tipo === 'Exterior');
     });
   }
 
